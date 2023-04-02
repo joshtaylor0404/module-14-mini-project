0dd0d459dfa380626b646c0a6d8bad097b1f6366
Josh Taylor <jtaylor4@bootcampspot.com>
2023-04-02 12:35:43 -0700

    woking login & logout routes and views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,4 +42,13 @@ router.get('/project/:id', async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
