4060da997e6abc91f46eb1d4e5a66905d175031b
Josh Taylor <jtaylor4@bootcampspot.com>
2023-04-02 12:05:13 -0700

    working project by ID route.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,4 +20,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
